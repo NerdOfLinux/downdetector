370d07c5c626d8e33b710d4716bd7c3bb4657970
NerdOfLinux <nerdoflinux@gateblogs.com>
2018-04-17 18:54:42 -0500

    Now displays HTTP response code and added some basic CSS

--- index.php
+++ index.php
@@ -4,12 +4,26 @@ $siteURL=$_GET['site'];
 <html>
 <head>
 	<title>Down Detector</title>
+	<style>
+		form{
+			text-align: center;
+		}
+		input{
+			font-size: 15px;
+		}
+		.data{
+			text-align: center;
+			font-size: 20px;
+			font-family: arial;
+		}
+	</style>
 </head>
 <body>
 <form name="downdetect" action="" method=GET>
 <input type="text" name="site" required value="<?php echo $siteURL;?>"> </input>
 <input type="submit" value="Check"></input>
 </form>
+<div class="data">
 <?php
 if(empty($siteURL)){
 	echo "No URL";
@@ -23,17 +37,18 @@ curl_setopt($cURL, CURLOPT_URL, "$siteURL");
 //Set options
 curl_setopt($cURL, CURLOPT_HEADER, 1);
 curl_setopt($cURL, CURLOPT_RETURNTRANSFER, 1);
+
 //Parse
 $headers=explode(" ",curl_exec($cURL));
 $status=$headers[1];
 if($status == "200"){
-	echo "Site is up!";
+	echo "Site is up!<br>Code: $status";
 }else if($status == "301" || $status == "302"){
-	echo "Site is redirecting!";
-}
-else{
-	echo "Site is down!";
+	echo "Site is redirecting!<br>Code: $status";
+}else{
+	echo "Site is down!<br>Code: $status";
 }
 ?>
+</div>
 </body>
 </html>
